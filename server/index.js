c0f152549750c0e2a616683565bae29ebbc8fe52
MD ARSALAN <mdarsalan@vibein.com>
2026-04-23 20:54:03 +0530

    Update DB env and add .htaccess for routing

--- server/index.js
+++ server/index.js
@@ -36,7 +36,7 @@ app.use(express.json());
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    password: process.env.DB_PASS,
+    password: process.env.DB_PASS || process.env.DB_PASSWORD,
     database: process.env.DB_NAME, // LOCK the database here
     waitForConnections: true,
     connectionLimit: 10,
